ae2f6e0029379d991802e084454f04dcf5d12edd
Jan Macheta <jan.macheta@embe.tech>
2024-01-03 18:43:57 +0100

    restore installation target

---
+++ cmake/logger_install.cmake
@@ -0,0 +1,25 @@
+include(CMakePackageConfigHelpers)
+include(GNUInstallDirs)
+
+write_basic_package_version_file(logger-config-version.cmake COMPATIBILITY SameMajorVersion)
+
+configure_package_config_file(
+  ${PROJECT_SOURCE_DIR}/cmake/logger-config.cmake.in ${CMAKE_CURRENT_BINARY_DIR}/logger-config.cmake
+  INSTALL_DESTINATION cmake/logger
+)
+
+install(TARGETS logger EXPORT logger_targets)
+
+install(DIRECTORY include/embetech DESTINATION ${CMAKE_INSTALL_INCLUDEDIR})
+
+install(FILES ${CMAKE_CURRENT_BINARY_DIR}/logger-config.cmake ${CMAKE_CURRENT_BINARY_DIR}/logger-config-version.cmake
+        DESTINATION cmake/logger
+)
+
+install(FILES ${PROJECT_SOURCE_DIR}/cmake/logger_utils.cmake DESTINATION cmake/logger)
+
+install(
+  EXPORT logger_targets
+  NAMESPACE embetech::
+  DESTINATION cmake/logger
+)
