3ec0683a8c0dd3bb67c6892dd08dd828da8f1033
Jan Macheta <jan.macheta@embe.tech>
2024-01-03 13:08:42 +0100

    fix requiements

--- requirements.cmake
+++ requirements.cmake
@@ -2,5 +2,9 @@ cmake_policy(SET CMP0135 NEW)
 
 include(FetchContent)
 
-# Optional - needed only when building tests is enabled
-FetchContent_Declare(googletest URL https://github.com/google/googletest/archive/main.zip FIND_PACKAGE_ARGS NAMES GTest)
+if (CONFIG_LOGGER_BUILD_TESTS)
+  # Optional - needed only when building tests is enabled
+  FetchContent_Declare(
+    googletest URL https://github.com/google/googletest/archive/main.zip FIND_PACKAGE_ARGS NAMES GTest
+  )
+endif ()
