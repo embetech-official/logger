3daef0ea74ff2e0136e9eff84961ad0d163b6582
Jan Macheta <jan.macheta@embe.tech>
2024-01-04 14:43:18 +0100

    improved unit tests

--- cmake/logger_install.cmake
+++ cmake/logger_install.cmake
@@ -1,11 +1,13 @@
 include(CMakePackageConfigHelpers)
 include(GNUInstallDirs)
 
+set(CMAKES_EXPORT_DIR cmake/logger)
+
 write_basic_package_version_file(logger-config-version.cmake COMPATIBILITY SameMajorVersion)
 
 configure_package_config_file(
   ${PROJECT_SOURCE_DIR}/cmake/logger-config.cmake.in ${CMAKE_CURRENT_BINARY_DIR}/logger-config.cmake
-  INSTALL_DESTINATION cmake/logger
+  INSTALL_DESTINATION ${CMAKES_EXPORT_DIR}
 )
 
 install(TARGETS logger EXPORT logger_targets)
@@ -13,7 +15,7 @@ install(TARGETS logger EXPORT logger_targets)
 install(DIRECTORY include/embetech DESTINATION ${CMAKE_INSTALL_INCLUDEDIR})
 
 install(FILES ${CMAKE_CURRENT_BINARY_DIR}/logger-config.cmake ${CMAKE_CURRENT_BINARY_DIR}/logger-config-version.cmake
-        DESTINATION cmake/logger
+        DESTINATION ${CMAKES_EXPORT_DIR}
 )
 
 install(FILES ${PROJECT_SOURCE_DIR}/cmake/logger_utils.cmake DESTINATION cmake/logger)
@@ -21,5 +23,5 @@ install(FILES ${PROJECT_SOURCE_DIR}/cmake/logger_utils.cmake DESTINATION cmake/l
 install(
   EXPORT logger_targets
   NAMESPACE embetech::
-  DESTINATION cmake/logger
+  DESTINATION ${CMAKES_EXPORT_DIR}
 )
