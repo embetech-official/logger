3b8c6806b2967dda62ab3fcde6ff3582a4b511b7
Jan Macheta <jan.macheta@embe.tech>
2024-01-04 19:53:42 +0100

    options rename

--- requirements.cmake
+++ requirements.cmake
@@ -2,7 +2,7 @@ cmake_policy(SET CMP0135 NEW)
 
 include(FetchContent)
 
-if (CONFIG_LOGGER_BUILD_TESTS)
+if (CONFIG_LOGGER_TESTS)
   # Optional - needed only when building tests is enabled
   FetchContent_Declare(
     googletest URL https://github.com/google/googletest/archive/main.zip FIND_PACKAGE_ARGS NAMES GTest
