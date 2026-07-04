a9614b251f3c62b830631ea92b3c2f2101292e7f
Jan Macheta <2372096+jmacheta@users.noreply.github.com>
2026-07-05 01:27:07 +0200

    deprecate logger_config.h instead of removing support
    logger_config.h is still auto-included via __has_include for
    backwards compatibility, but now emits a compiler warning pointing
    users to logger_set_max_level in CMake.

--- src/include/embetech/logger.h
+++ src/include/embetech/logger.h
@@ -10,6 +10,11 @@
 #ifndef LOGGER_H_
 #define LOGGER_H_
 
+#if (!defined LOGGER_MAIN_) && __has_include(<logger_config.h>)
+#    warning "logger_config.h is deprecated, use logger_set_max_level in CMake instead"
+#    include <logger_config.h>
+#endif
+
 #include <inttypes.h>
 #include <stdbool.h>
 #include <stddef.h>
